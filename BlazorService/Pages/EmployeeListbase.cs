65eafed76c8c0e7d69231078cc439687821b86ca
Samikshya <write2samikshya@gmail.com>
2021-05-20 08:32:19 +0530

    --next

---
+++ BlazorService/Pages/EmployeeListbase.cs
@@ -0,0 +1,18 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorService.Pages
+{
+    public class EmployeeListbase:ComponentBase
+    {
+
+        IEnumerable<Employee> Employees { get; set; }
+
+
+
+    }
+}
