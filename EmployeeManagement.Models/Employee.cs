1121899a93c02f7762f33a90f14a7a399feff76f
Samikshya <write2samikshya@gmail.com>
2021-05-20 06:58:54 +0530

    -- Second Commit

---
+++ EmployeeManagement.Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeManagement.Models
+{
+   public  class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string EmpName { get; set; }
+
+        public Department Department { get; set; }
+
+        public Gender Gender { get; set; }
+
+        public DateTime DateofBirth { get; set; }
+
+        public string Email { get; set; }
+
+        public string Photopath { get; set; }
+
+    }
+}
